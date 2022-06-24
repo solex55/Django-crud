b46d2ce059dbe95b3864570b1e18cde6d54f431e
Solomon Achugwoh <52291436+solex55@users.noreply.github.com>
2022-06-24 18:28:19 +0100

    Create views.py

---
+++ I4G004064J3Z/blog/views.py
@@ -0,0 +1,37 @@
+from django.views import generic
+
+from .models import Post
+
+from django.urls import reverse_lazy
+
+class PostListView(generic.ListView):
+
+    model = Post
+
+class PostCreateView(generic.CreateView):
+
+    model = Post
+
+    fields = "__all__"
+
+    success_url  = reverse_lazy("blog:all")
+
+class  PostDetailView(generic.DetailView):
+
+    model = Post
+
+class  PostUpdateView(generic.UpdateView):
+
+    model = Post
+
+    fields = "__all__"
+
+    success_url  = reverse_lazy("blog:all")
+
+class  PostDeleteView(generic.DeleteView):
+
+    model = Post
+
+    fields = "__all__"
+
+    success_url  = reverse_lazy("blog:all")
