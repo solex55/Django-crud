095223b1ee503c1ab822d3f9e6b3b487d9581e73
Solomon Achugwoh <52291436+solex55@users.noreply.github.com>
2022-06-24 18:23:22 +0100

    Create wsgi.py

---
+++ I4G004064J3Z/I4G004064J3Z/wsgi.py
@@ -0,0 +1,20 @@
+"""
+
+WSGI config for I4G004064J3Z project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G004064J3Z.settings')
+
+application = get_wsgi_application()
+
